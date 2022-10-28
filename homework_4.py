43d52299f707761705578355f8b4873bd4ae7286
IgorShkatulo <shkatulo59@gmail.com>
2022-10-28 10:45:58 +0500

    переделанное дз

--- homework_4.py
+++ homework_4.py
@@ -36,44 +36,17 @@
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 
 
-# from random import randint
-# import random
-#
-# k = int(input('Введите натуральную степень k: '))
-# while k <= 0:
-#     k = int(input('Введены не корректные данные! Введите натуральную степень k: '))
-#
-# result = [0 for i in range(k)]
-# koef = random.sample(range(0, 101), k + 1)
-# print(f'Рандомные коэффициенты: {koef}')
-# for i in range(len(result)):
-#     result[i] = f'{koef[i]}x^{k}'
-#     k -= 1
-# result.append(str(koef[-1]))
-#
-#
-# print(f'До обработки: {result}')
-# for elem in result:
-#     if elem == 0:
-#         result.remove(elem)
-#     try:
-#         ind_x = elem.find('x')
-#         d = int(elem[:ind_x])
-#     except AttributeError:
-#         continue
-#     if d == 0 or elem == '0':
-#         result.remove(elem)
-#     if '^1' in elem:
-#         result.remove(elem)
-# result.insert(-1, elem[:elem.find('^1')])
-#
-#
-# print(f'После обработки: {result}')
-# polynom = ''
-# for i in range(len(result) - 1):
-#     polynom += f'{result[i]} + '
-# polynom += f'{result[-1]} = 0'
-# print(polynom)
-#
-# with open('text.txt', 'w') as f:
-#     f.write(polynom)
+import random
+
+some_dict = {0: '⁰', 1: '¹', 2: '²', 3: '³', 4: '⁴', 5: '⁵', 6: '⁶', 7: '⁷', 8: '⁸', 9: '⁹'}
+k = int(input('Введите натуральную степень k: '))
+coef = [random.randint(0, 100) for _ in range(k + 1)]
+print(coef)
+with open('coef.txt', 'w', encoding='utf-8') as m:
+    for i in range(len(coef)):
+        if k - i != 1 and k - i != 0:
+            m.write(f'{coef[i]}x{some_dict[k - i]} + ')
+        elif k - i == 1:
+            m.write(f'{coef[i]}x + ')
+        elif k - i == 0:
+            m.write(f'{coef[i]} = 0')
